eff09a739aecb446a354e50dca7a43a0d6a0d82b
Nimesh <nmaslekar777@gmail.com>
2024-01-14 16:55:20 +0530

    initial commit #test

---
+++ app.js
@@ -0,0 +1,32 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app =  express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+var items = ["Buy Vegetables", "Cook food", "Eat Food"];
+var item = "";
+
+app.get("/",function(req,res){
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today = new Date();
+    var day = today.toLocaleDateString("en-US",options);
+    res.render("list",
+    {
+        listTitle:day,
+        newItems:items
+    });
+});
+
+app.post("/",function(req,res){
+    item = req.body.newItem ;
+    items.push(item);
+    res.redirect("/");
+})
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+});
